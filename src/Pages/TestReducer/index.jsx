256de6142ff6c7f5c8486a305df4510ede1c68d5
Ilqar <bakinitygames.94@gmail.com>
2023-02-13 17:21:38 +0400

    Comlete_and_Add

--- src/Pages/TestReducer/index.jsx
+++ src/Pages/TestReducer/index.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useReducer } from "react";
+import "./style.css";
+
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: true,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    complete: false,
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "COMPLETE": {
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      });
+    }
+    case "ADD": {
+      return [
+        ...state,
+        { ...action.data, complete: true, id: state.length + 1 },
+      ];
+    }
+
+    case "REMOVE": {
+      return [];
+    }
+
+    default:
+      return state;
+  }
+};
 
 const Index = () => {
-  return <div className='TestReducer'> TestReducer</div>;
+  const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+  const completeChanger = (value) => {
+    dispatch({ type: "COMPLETE", id: value.id });
+  };
+
+  const addInputHandler = (e) => {
+    const { keyCode, target } = e;
+
+    if (keyCode === 13) {
+      dispatch({ type: "ADD", data: { title: target.value } });
+      target.value = "";
+    }
+  };
+
+  const deleteInputHandler = (value) => {};
+
+  return (
+    <div className='TestReduser'>
+      <input placeholder='Add Todo' onKeyDown={addInputHandler} />
+      <ol className='TestReducer'>
+        {todos.map((value, index) => (
+          <li key={index} className={!value.complete ? "li" : ""}>
+            <label>
+              <input
+                type='checkbox'
+                checked={!value.complete}
+                onChange={() => completeChanger(value)}
+              ></input>
+              {value.title}
+              &nbsp;
+              <button onClick={deleteInputHandler}>Delete</button>
+            </label>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Index;
